8765d2522d9ca783b20fbc72696fbabe0696b813
kenanarslan <kenan.depo@gmail.com>
2022-12-07 20:01:16 +0100

    Initial commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TodosComponent} from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
+    //you can add components here in Declaratios, so you can use them
   ],
   imports: [
+    //by adding the modules here which are imported/defined in the upper part of this file you can use them
     BrowserModule,
     AppRoutingModule
   ],
